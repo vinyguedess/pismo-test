49c9d81d08f7047c3853f69a5243f52aa347c8a7
Vinicius Guedes <viniciusgued@gmail.com>
2023-08-01 01:05:58 -0300

    feat(handlers): create_account_handler

---
+++ handlers/create_account_handler.go
@@ -0,0 +1,60 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"pismo/entities"
+	"pismo/models"
+	"pismo/services"
+)
+
+type createAccountHandler struct {
+	accountService services.AccountService
+}
+
+func NewCreateAccountHandler(accountService services.AccountService) Handler {
+	return &createAccountHandler{
+		accountService: accountService,
+	}
+}
+
+func (h *createAccountHandler) Route() string {
+	return "/accounts"
+}
+
+func (h *createAccountHandler) Method() []string {
+	return []string{http.MethodPost}
+}
+
+func (h *createAccountHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var payload models.Account
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		err = entities.NewError("Invalid JSON", []string{err.Error()})
+
+		jsonPayload, _ := json.Marshal(err)
+		w.Write(jsonPayload)
+		return
+	}
+
+	account, err := h.accountService.Create(r.Context(), payload)
+	if err != nil {
+		switch err.(type) {
+		case *entities.AccountAlreadyExistsError:
+			w.WriteHeader(http.StatusConflict)
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+			err = entities.NewError("Internal server error", []string{err.Error()})
+		}
+
+		jsonPayload, _ := json.Marshal(err)
+		w.Write(jsonPayload)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("ETag", account.ID.String())
+	w.Header().Set("Location", fmt.Sprintf("/accounts/%s", account.ID.String()))
+}
---
+++ handlers/create_account_handler_test.go
@@ -0,0 +1,116 @@
+package handlers
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/suite"
+
+	"pismo/entities"
+	mock_services "pismo/mocks/services"
+	"pismo/models"
+)
+
+type createAccountHandlerTestSuite struct {
+	suite.Suite
+	ctrl            *gomock.Controller
+	authServiceMock *mock_services.MockAccountService
+	handler         Handler
+}
+
+func TestCreateAccountHandlerTestSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(createAccountHandlerTestSuite))
+}
+
+func (s *createAccountHandlerTestSuite) SetupTest() {
+	s.ctrl = gomock.NewController(s.T())
+	s.authServiceMock = mock_services.NewMockAccountService(s.ctrl)
+	s.handler = NewCreateAccountHandler(s.authServiceMock)
+}
+
+func (s *createAccountHandlerTestSuite) TestRoute() {
+	s.Equal("/accounts", s.handler.Route())
+}
+
+func (s *createAccountHandlerTestSuite) TestMethod() {
+	s.Equal([]string{http.MethodPost}, s.handler.Method())
+}
+
+func (s *createAccountHandlerTestSuite) TestServeHTTP() {
+	accountId := uuid.New()
+	account := &models.Account{
+		BaseModel: models.BaseModel{
+			ID: accountId,
+		},
+	}
+
+	tests := []struct {
+		description             string
+		payload                 string
+		invalidPayloadErr       bool
+		responseCreate          *models.Account
+		errCreate               error
+		accountAlreadyExistsErr bool
+		expectedResponse        string
+		expectedStatusCode      int
+	}{
+		{
+			description:        "Success",
+			payload:            `{"document_number": "123456789"}`,
+			responseCreate:     account,
+			expectedStatusCode: http.StatusCreated,
+		},
+		{
+			description:        "Invalid payload",
+			payload:            `{"document_number": "123456789"`,
+			invalidPayloadErr:  true,
+			expectedResponse:   `{"message":"Invalid JSON","details":["unexpected EOF"]}`,
+			expectedStatusCode: http.StatusUnprocessableEntity,
+		},
+		{
+			description:             "Account already exists",
+			payload:                 `{"document_number": "123456789"}`,
+			errCreate:               entities.NewAccountAlreadyExistsError("123456789"),
+			accountAlreadyExistsErr: true,
+			expectedResponse:        `{"message":"Account already exists","details":["123456789"]}`,
+			expectedStatusCode:      http.StatusConflict,
+		},
+		{
+			description:        "Internal server error",
+			payload:            `{"document_number": "123456789"}`,
+			errCreate:          errors.New("Unexpected error"),
+			expectedResponse:   `{"message":"Internal server error","details":["Unexpected error"]}`,
+			expectedStatusCode: http.StatusInternalServerError,
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.description, func() {
+			request := httptest.NewRequest(http.MethodPost, "/accounts", bytes.NewReader([]byte(test.payload)))
+			response := httptest.NewRecorder()
+
+			if !test.invalidPayloadErr {
+				s.authServiceMock.EXPECT().
+					Create(request.Context(), models.Account{DocumentNumber: "123456789"}).
+					Return(test.responseCreate, test.errCreate)
+			}
+
+			s.handler.ServeHTTP(response, request)
+
+			s.Equal(test.expectedStatusCode, response.Code)
+			if test.responseCreate == nil {
+				s.Equal(test.expectedResponse, response.Body.String())
+			} else {
+				s.Equal(accountId.String(), response.Header().Get("ETag"))
+				s.Equal(fmt.Sprintf("/accounts/%s", accountId.String()), response.Header().Get("Location"))
+			}
+		})
+	}
+}
